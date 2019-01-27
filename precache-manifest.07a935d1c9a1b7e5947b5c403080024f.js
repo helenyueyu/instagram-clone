de6b5712a00d99c0010b483299248964c90cff64
tokyosuite <helenyueyu@gmail.com>
2019-01-27 15:33:55 -0800

    Updates

---
+++ precache-manifest.07a935d1c9a1b7e5947b5c403080024f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30b08733495760b01197",
+    "url": "/instagram-clone/static/css/main.ef2c6a52.chunk.css"
+  },
+  {
+    "revision": "30b08733495760b01197",
+    "url": "/instagram-clone/static/js/main.30b08733.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/instagram-clone/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "49f606084b0531baf4c7",
+    "url": "/instagram-clone/static/js/runtime~main.49f60608.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/instagram-clone/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cd708887fd9215618af2e9d57a3b5b8d",
+    "url": "/instagram-clone/index.html"
+  }
+];
