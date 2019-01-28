04155879106c47472e9aba18b4387b15a53ba1bc
tokyosuite <helenyueyu@gmail.com>
2019-01-27 16:53:55 -0800

    Finally got the error messages of failed FETCH requests to the API to no longer show up in the API as broken image links.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// url: https://picsum.photos/500/500
+
+
 class App extends Component {
+  state = {
+    images: [],
+    err: []
+  }
+  componentDidMount() {
+    for (let i = 15; i < 115; i++) {
+      fetch(`https://picsum.photos/500/500/?image=${i}`)
+        .then(function(response) {
+          if (!response.ok) {
+            throw Error(response.statusText)
+          }
+          return response
+        })
+        .then(res => this.setState({
+          images: this.state.images.concat(res.url)
+        }))
+        .catch(err => console.log(err))
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.images.map((x, idx) => <img key={idx} src={x} alt={idx} width="350px"/>)}
       </div>
     );
   }
